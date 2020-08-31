e01f346f95c446ddaa0bf61e55ce3b960b02105c
Kevin <kevin.chu215@gmail.com>
2020-08-31 16:19:55 +1000

    Releasing Commented Code

--- Production/Back_End/application.py
+++ Production/Back_End/application.py
@@ -19,29 +19,29 @@ cors = CORS(application)
 application.config['CORS_HEADERS'] = 'Content-Type'
 cache = Cache(application, config={'CACHE_TYPE': 'redis', 'CACHE_REDIS_URL': 'redis://steamscoutredis.xb4wpu.ng.0001.use1.cache.amazonaws.com:6379'})
 
-# @application.route('/all_games')
-# def all_game():
-#     database = SteamDatabase(False)
-#     results = database.query_database('SELECT name, icon FROM "Game"')
-#     database.shutdown()
+@application.route('/all_games')
+def all_game():
+    database = SteamDatabase(False)
+    results = database.query_database('SELECT name, icon FROM "Game"')
+    database.shutdown()
 
-#     return dumps(results)
+    return dumps(results)
 
-# @application.route('/all_items/<game>')
-# def all_items_in_game(game):
-#     database = SteamDatabase(False)
-#     results = database.query_database(f"SELECT * FROM \"Item\" WHERE name='{game}'"")
-#     database.shutdown()
+@application.route('/all_items/<game>')
+def all_items_in_game(game):
+    database = SteamDatabase(False)
+    results = database.query_database(f"SELECT * FROM \"Item\" WHERE name='{game}'"")
+    database.shutdown()
 
-#     return dumps(results)
+    return dumps(results)
 
-# @application.route('/all_prices/<app_id>/<item>/<type>')
-# def all_prices_for_item_in_game(app_id, item, type):
-#     database = SteamDatabase(False)
-#     results = database.query_database(f"SELECT market_hash_name, time, median_price, volume FROM \"{type}\" WHERE market_hash_name='{database.clean_market_hash_name(item)}'")
-#     database.shutdown()
+@application.route('/all_prices/<app_id>/<item>/<type>')
+def all_prices_for_item_in_game(app_id, item, type):
+    database = SteamDatabase(False)
+    results = database.query_database(f"SELECT market_hash_name, time, median_price, volume FROM \"{type}\" WHERE market_hash_name='{database.clean_market_hash_name(item)}'")
+    database.shutdown()
 
-#     return dumps(results, default=str)
+    return dumps(results, default=str)
 
 @application.route("/test")
 def test():
@@ -58,7 +58,7 @@ def echo_back(echo):
     """
     return f"You Typed: {echo} {randint(3, 13300)}"
 
-@application.route("/market_overview/")
+@application.route("/market_overview")
 @cache.cached(timeout=3600)
 def market_overview():
     """
@@ -85,7 +85,7 @@ def market_overview():
 
     return dumps(results, default=str)
 
-@application.route("/transaction_amount/")
+@application.route("/transaction_amount")
 @cache.cached(timeout=3600)
 def transaction_amount():
     """
@@ -101,7 +101,7 @@ def transaction_amount():
 
     return dumps(results, default=str)
 
-@application.route("/market_seasonality/")
+@application.route("/market_seasonality")
 @cached.cached(timeout=3600)
 def market_seasonality():
     """
@@ -112,7 +112,7 @@ def market_seasonality():
     database = SteamDatabase(False)
     results = database.query_database("""
         SELECT start_month_prices.month, AVG(((end_month_prices.median_price - start_month_prices.median_price) / start_month_prices.median_price) * 100) as seasonal_percentage_move
-        FROM (SELECT daily_price_action.month, daily_price_action.year, daily_price_action.median_price
+        FROM (SELECT daily_price_action.month, daily_price_action.year, daily_price_action.median_price, daily_price_action.market_hash_name
             FROM (SELECT max(day) as day, month, year, market_hash_name
                 FROM
                 (SELECT date_part('day', time) as day, date_part('month', time) as month, date_part('year', time) as year, market_hash_name
@@ -125,9 +125,9 @@ def market_seasonality():
                 AND daily_price_action.day = end_month_data.day
                 AND daily_price_action.month = end_month_data.month
                 AND daily_price_action.year = end_month_data.year
-            GROUP BY daily_price_action.month, daily_price_action.year, daily_price_action.median_price) as end_month_prices
+            GROUP BY daily_price_action.month, daily_price_action.year, daily_price_action.median_price, daily_price_action.market_hash_name) as end_month_prices
             JOIN
-            (SELECT daily_price_action.month, daily_price_action.year, daily_price_action.median_price
+            (SELECT daily_price_action.month, daily_price_action.year, daily_price_action.median_price, daily_price_action.market_hash_name
             FROM (SELECT min(day) as day, month, year, market_hash_name
                 FROM
                 (SELECT date_part('day', time) as day, date_part('month', time) as month, date_part('year', time) as year, market_hash_name
@@ -140,9 +140,10 @@ def market_seasonality():
                 AND daily_price_action.day = start_month_data.day
                 AND daily_price_action.month = start_month_data.month
                 AND daily_price_action.year = start_month_data.year
-            GROUP BY daily_price_action.month, daily_price_action.year, daily_price_action.median_price) as start_month_prices
+            GROUP BY daily_price_action.month, daily_price_action.year, daily_price_action.median_price, daily_price_action.market_hash_name) as start_month_prices
             ON start_month_prices.month = end_month_prices.month
             AND start_month_prices.year = end_month_prices.year
+            AND start_month_prices.market_hash_name = end_month_prices.market_hash_name
         GROUP BY start_month_prices.month
     """)
 
